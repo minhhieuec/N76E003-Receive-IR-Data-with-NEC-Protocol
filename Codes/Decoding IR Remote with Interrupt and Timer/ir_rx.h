ed3ba02928bef9dbcedbebc5ebcfed00d41539fb
minhhieuec <minhhieuec@gmail.com>
2020-05-08 21:10:29 +0700

    receive 4 byte ir data via ext1

--- Codes/Decoding IR Remote with Interrupt and Timer/ir_rx.h
+++ Codes/Decoding IR Remote with Interrupt and Timer/ir_rx.h
@@ -6,9 +6,17 @@
 #include "Common.h"
 #include "Delay.h"
 
-void ir_rx_setup(void);
+#define IR_DATA_LEN 4 //  8 bytes
+
+typedef enum
+{
+    IR_USE_PIN_IT = 0, // use pin interrupt
+    IR_USE_EXT0_IT,    // use external interrupt 0
+    IR_USE_EXT1_IT,    // use external interrupt 1
+} IR_Type_t;
+
+void ir_rx_setup(IR_Type_t ir_type);
 uint8_t is_ir_data_received(void);
-void erase_frames(void);
-void decode_NEC_IR(void);
+void get_ir_data(uint8_t *buf, uint8_t len);
 
 #endif
--- Codes/Decoding IR Remote with Interrupt and Timer/main.c
+++ Codes/Decoding IR Remote with Interrupt and Timer/main.c
@@ -13,22 +13,77 @@
 #define delay_ms(x) Timer2_Delay500us(x * 2)
 #define debug printf
 
-char putchar(char c) {
+uint8_t ir_data[IR_DATA_LEN];
+uint8_t led_stt[3] = {0, 0, 0};
+
+char putchar(char c)
+{
   while (!TI)
     ;
   TI = 0;
   return (SBUF = c);
 }
 
-void main(void) {
+void main(void)
+{
+  uint8_t i = 0;
+
   InitialUART0_Timer1(115200);
   printf("main \r\n");
-  
-  ir_rx_setup();
 
-  while (1) {
-    if (is_ir_data_received()) {
-      decode_NEC_IR();
+  //P05_Input_Mode;
+//Enable_INT_Port0;
+  // Enable_BIT5_LowLevel_Trig;
+  //Enable_BIT5_FallEdge_Trig;
+
+  ir_rx_setup(IR_USE_EXT1_IT);
+
+  while (1)
+  {
+    if (is_ir_data_received())
+    {
+      get_ir_data(&ir_data[0], IR_DATA_LEN);
+
+      if (ir_data[2] == 0x30 && ir_data[3] == 0xCF)
+      {
+        led_stt[0] = ~led_stt[0];
+        printf("P12: %d \n", (uint16_t)led_stt[0]);
+        if (led_stt[0])
+          P12 = 1;
+        else
+          P12 = 0;
+      }
+
+      if (ir_data[2] == 0x18 && ir_data[3] == 0xE7)
+      {
+        led_stt[1] = ~led_stt[1];
+        printf("P03: %d \n", (uint16_t)led_stt[1]);
+        if (led_stt[1])
+          P03 = 1;
+        else
+          P03 = 0;
+      }
+
+      if (ir_data[2] == 0x7A && ir_data[3] == 0x85)
+      {
+        led_stt[2] = ~led_stt[2];
+        printf("P04: %d \n", (uint16_t)led_stt[2]);
+        if (led_stt[2])
+          P04 = 1;
+        else
+          P04 = 0;
+      }
+
+      delay_ms(1);
+
+      printf("ir_rec: ");
+      for (i = 0; i < IR_DATA_LEN; i++)
+      {
+        printf("%x ", (uint16_t)ir_data[i]);
+      }
+      printf("\n");
     }
-  };
+
+    delay_ms(10);
+  }
 }
